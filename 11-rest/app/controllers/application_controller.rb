4489857cba3a8ec53d148c2952e31e2f1982d94e
Graham Troyer-Joy <graham.troyer-joy@flatironschool.com>
2020-02-20 12:48:59 -0500

    sdfljksdfj

--- 11-rest/app/controllers/application_controller.rb
+++ 11-rest/app/controllers/application_controller.rb
@@ -5,17 +5,19 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :method_override, true
   end
+
 
   get "/" do
     redirect to "/vehicles"
   end
 
-
   get "/vehicles" do
     @vehicles = Vehicle.all
     erb :index
   end
+
   # def read_all # GET /view-vehicles
   #   @vehicles = Vehicle.all
   # end
@@ -23,10 +25,6 @@ class ApplicationController < Sinatra::Base
   get "/vehicles/new" do
     erb :new # render the erb for the form
   end
-
-  # def create 
-  #   @vehicle = Vehicle.create(make: "", model: "", color: "")
-  # end
 
   post "/vehicles" do
     # binding.pry
@@ -41,14 +39,13 @@ class ApplicationController < Sinatra::Base
   #   @vehicle = Vehicle.create(make: "", model: "", color: "")
   # end
 
-
-  # def read_all # GET /view-vehicles
-  #   @vehicles = Vehicle.all
-  # end
-
   get "/vehicles/:id" do
-    @vehicle = Vehicle.find(params[:id])
-    erb :show
+    begin
+      get_vehicle_by_id_in_params
+      erb :show
+    rescue ActiveRecord::RecordNotFound
+      404
+    end  
   end
 
   # def read_one
@@ -56,11 +53,33 @@ class ApplicationController < Sinatra::Base
   #   # @vehicle = Vehicle.find_by(make: "Honda")
   # end
 
+  get "/vehicles/:id/edit" do
+    get_vehicle_by_id_in_params
+    erb :edit
+  end
+
+  patch "/vehicles/:id" do
+    params.delete("_method")
+    get_vehicle_by_id_in_params
+    @vehicle.update(params)
+    redirect to "/vehicles/#{ @vehicle.id }"
+  end
+
   # def update
   #   # Vehicle.update({ 6 => { :make => "", model: "" } })
   #   @vehicle = Vehicle.find(5)
   #   @vehicle.update(make: "", model: "")
   # end
+
+  delete "/vehicles/:id" do
+    @vehicle = get_vehicle_by_id_in_params
+    @vehicle.destroy
+    redirect to "/vehicles"
+  end
+
+  def get_vehicle_by_id_in_params
+    @vehicle = Vehicle.find(params[:id])
+  end
 
   # def destroy 
   #   @vehicle = Vehicle.find(7)
