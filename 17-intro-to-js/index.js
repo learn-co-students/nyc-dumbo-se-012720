6da84f6f2cfb1e250334846efc81a367dd0e88ae
ihollander <ianholla@gmail.com>
2020-03-09 07:52:18 -0400

    before intro to js

---
+++ 17-intro-to-js/index.js
@@ -0,0 +1,15 @@
+/**** debugging ****/
+console.log('HELLO WORLD')
+console.log('%cHELLO WORLD', 'color: red')
+
+// debugger
+
+/**** data types ****/
+
+/**** truthy/falsey values ****/
+
+/**** pass by reference vs pass by value ****/
+
+/**** arrays and objects ****/
+
+/**** function definition vs invocation ****/
