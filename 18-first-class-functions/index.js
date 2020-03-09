82c3b8cefdb03fcd5b4921118a381cda46f457f2
Ian Hollander <ianholla@gmail.com>
2020-03-09 13:23:57 -0400

    before first class functions

---
+++ 18-first-class-functions/index.js
@@ -0,0 +1,37 @@
+// /******* Hoisting *******/
+function sayHi(name) {
+  console.log("hi " + name)
+  return 10
+}
+
+let a = sayHi("Randy")
+
+console.log(a)
+
+// /******* var, let, const, (global) *******/
+
+// /******* Lexical Scoping *******/
+
+// /******* global vs function vs block scope *******/
+
+// /******* scope in nested functions *******/
+
+
+
+
+
+
+
+
+
+
+
+// /******* 1st Class Functions *******/
+// 1. functions can be assigned to a variable
+
+// 2. functions can be passed around as arguments to other functions
+
+// Example: Array methods
+
+// 3. functions can be returned from functions
+
---
+++ 18-first-class-functions/ruby.rb
@@ -0,0 +1,8 @@
+def say_hi(name)
+  puts "hi " + name
+  10
+end
+
+a = say_hi("Randy")
+
+puts a # 10
