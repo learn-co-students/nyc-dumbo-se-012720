fa9d748c6dc9c06d4752417eef83610e1d79f275
Eric Kim <hyeokjungkim@gmail.com>
2020-04-16 15:45:49 -0400

    adds lecture

---
+++ 48-react-redux/src/chips.js
@@ -0,0 +1,22 @@
+let arrayOfChips = [
+
+  {
+    name: "Doritos",
+    brand: "Frito-Lay"
+  },
+  {
+    name: "Fritos",
+    brand: "Frito-Lay"
+  },
+  {
+    name: "Pringles",
+    brand: "Kellogg"
+  },
+  {
+    name: "Cheez-It",
+    brand: "Kellogg"
+  }
+  
+]
+
+export default arrayOfChips
---
+++ 48-react-redux/src/components/ChipForm.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
+
+class ChipForm extends Component {
+
+  state={
+    name: "",
+    brand: ""
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+ handleSubmit = (e) => {
+   e.preventDefault()
+   this.props.addOneChip(this.state)
+ }
+
+ render() {
+   let {name, brand} = this.state
+   return (
+     <form onSubmit={this.handleSubmit}>
+       <label htmlFor="name">
+         Name
+       </label>
+         <input type="text"
+            id="name"
+            name="name"
+            autoComplete="off"
+            value={name}
+            onChange={this.handleChange}
+        />
+       <label htmlFor="brand">
+         Brand
+       </label>
+         <input type="text"
+            id="brand"
+            name="brand"
+            value={brand}
+            autoComplete="off"
+            onChange={this.handleChange}
+          />
+       <input type="submit"/>
+     </form>
+   );
+ }
+
+}
+
+
+// first argument of connect is mapStateToProps
+  // mapStateToProps is a function that will take in reduxState and make them the props (Getting Information)
+
+// second argument of connect is mapDispatchToProps
+  // mapDispatchToProps is a POJO that takes in values of function definitions (Sending information)
+      // each function definition will correspond to an action
+  // mapDispatchToProps as a POJO will get merged into props
+
+
+
+
+// actionCreator
+
+let addOneChip = (newChipObject) => {
+
+  let theAction = { type: "ADD_ONE_CHIP", payload: newChipObject }
+
+  return theAction
+}
+
+
+let mapDispatchToProps = {
+  addOneChip: addOneChip
+}
+
+export default connect(null, mapDispatchToProps)(ChipForm);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
